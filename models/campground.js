c907e747f02b5f6db9e1074ba165fc6cf05e534a
EC2 Default User <ec2-user@ip-172-31-17-142.us-east-2.compute.internal>
2019-02-14 02:19:27 +0000

    initial deploy commit

---
+++ models/campground.js
@@ -0,0 +1,35 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   price: String,
+   image: String,
+   description: String,
+   created_at: { type: Date, default: Date.now },
+   price: Number,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+// Getter
+campgroundSchema.path('price').get(function (num) {
+   return (num / 100).toFixed(2);
+});
+
+// Setter
+campgroundSchema.path('price').set(function (num) {
+   return num * 100;
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
